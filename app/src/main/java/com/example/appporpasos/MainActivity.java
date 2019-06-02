659051b000e45a87bf72acef9a1a1bf1883ade4c
ogaliz <oscargalvezr@live.com>
2019-06-02 14:03:12 +0200

    Comit intermedio en el que construimos una ActionBar para nuestra app con funciones de ejemplo, antes de cambiarla por la más moderna Toolbar

--- app/src/main/java/com/example/appporpasos/MainActivity.java
+++ app/src/main/java/com/example/appporpasos/MainActivity.java
@@ -44,15 +44,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//  Creamos los dos metodos para crear el menu y para configurar las acciones
+//  Creamos los dos metodos para crear el menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_en_activity, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+//  Creamos el método para configurar las acciones
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+//      Estamos capturando el id del elemento del menú para establecer la acción
+        int id = item.getItemId();
+        if (id == R.id.configuracion){
+            Toast.makeText(MainActivity.this, "Has pulsado configuración", Toast.LENGTH_LONG).show();
+            return  true;
+        }
+        if (id == R.id.informacion){
+            Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+            startActivity(intent);
+            return  true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
